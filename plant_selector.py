b20ad600ed4f1798154b31ab703052351b1bab75
Max <mbott@redhat.com>
2016-01-24 07:02:09 +1100

    Added additional code to connect to the underlying database, perform an sql query and display the results in a table. -Added extra layers to the spatial query

--- plant_selector.py
+++ plant_selector.py
@@ -20,9 +20,11 @@
  *                                                                         *
  ***************************************************************************/
 """
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, QObject, SIGNAL
-from PyQt4.QtGui import QAction, QIcon
-from qgis.core import *
+from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, QObject, SIGNAL, QVariant
+from PyQt4.QtGui import QAction, QIcon, QMessageBox, QTableWidget, QTableWidgetItem
+from PyQt4.QtSql import QSqlDatabase, QSqlQuery
+#from Qt import *
+from qgis.core import QgsMapLayerRegistry, QgsGeometry, QgsFeatureRequest, QgsDataSourceURI
 from qgis.gui import QgsMapToolEmitPoint
 # Initialize Qt resources from file resources.py
 import resources
@@ -64,6 +66,8 @@ class PlantSelector:
         self.dlg = PlantSelectorDialog()
         # reference to map canvas
         self.canvas = self.iface.mapCanvas()
+        # reference to named layers
+        # pHlayer = QgisMapLayerRegistry.instance().mapLayersByName('pH')
 
         # Declare instance attributes
         self.actions = []
@@ -74,11 +78,16 @@ class PlantSelector:
         # the identify tool will emit a QgsPoint on every click
         self.clickTool = QgsMapToolEmitPoint(self.canvas)
         # create a list to hold our selected feature ids
-        self.selectList = []
-        # current layer ref (set in handleLayerChange)
-        self.cLayer = None
-        # current layer dataProvider ref (set in handleLayerChange)
-        self.provider = None
+        self.phList = []
+        self.MoistureList = []
+        # Soil Property Layers
+        self.pHLayer = None
+        self.MoistureLayer = None
+        self.NitrogenLayer = None
+        # Above Ground Layers
+        self.WindLayer = None
+        self.LightLayer = None
+
 
 
 
@@ -203,7 +212,15 @@ class PlantSelector:
 
     def handleMouseDown(self, point, button):
         self.dlg.clearTextBrowser()
-        self.dlg.setTextBrowser( str(point.x()) + " , " +str(point.y()) )
+        self.dlg.setTextBrowser(str(point.x()) + " , " + str(point.y()))
+        # find out map coordinates from mouse click
+        #mapPoint = self.toLayerCoordinates(layer, event.pos())
+        #tolerance = self.plugin.getTolerance(layer)
+        #area = QgsRectangle(mapPoint.x() - tolerance, mapPoint.y() - tolerance, mapPoint.x() + tolerance, mapPoint.y() + tolerance)
+
+        #request = QgsFeatureRequest()
+        #request.setFilterRect(area).setFlags(QgsFeatureRequest.ExactIntersect)
+        #request.setSubsetOfAttributes([0])
 
     def handleLayerChange(self, layer):
         self.cLayer = self.canvas.currentLayer()
@@ -213,47 +230,130 @@ class PlantSelector:
 
     def updateTextBrowser(self):
         # check to make sure we have a feature selected in our selectList -- note that there might be more than one feature
-        if self.selectList:
+        phoutput = ""
+        moistureoutput = ""
+        if self.phList:
 
             # ############ EXAMPLE 1 EDITS GO HERE ####################
             ''' write code that will output ALL selected feature attributes for a single feature into the Text Browser'''
             ''' instead of using the dataProvider.select() function get the actual QgsFeature using dataProvider.featureAtId() '''
             # get the feature by passing in empty Feature
-            request = QgsFeatureRequest(self.selectList[0])
-            for f in self.cLayer.getFeatures(request):
-                output = "FEATURE ID: %i\n\t %s " % (f['id'],f['pH'])
+            request = QgsFeatureRequest(self.phList[0])
+            for f in self.pHLayer.getFeatures(request):
+                phoutput = "Soil pH Level: %s \n" % (f['pH'])
 
-                self.dlg.setTextBrowser(output)
+        if self.MoistureList:
+            request = QgsFeatureRequest(self.MoistureList[0])
+            for f in self.MoistureLayer.getFeatures(request):
+                moistureoutput = "Soil Moisture Level: %s \n" % (f['moisture_level'])
+
+        output = phoutput + moistureoutput
+        self.dlg.setTextBrowser(output)
+
+        provider = self.pHLayer.dataProvider()
+        if provider.name() == 'postgres':
+            # get the URI containing the connection parameters
+            uri = QgsDataSourceURI(provider.dataSourceUri())
+            print uri.uri()
+            # create a PostgreSQL connection using QSqlDatabase
+            db = QSqlDatabase.addDatabase('QPSQL')
+            # check to see if it is valid
+            if db.isValid():
+                print "QPSQL db is valid"
+                # set the parameters needed for the connection
+                db.setHostName(uri.host())
+                db.setDatabaseName(uri.database())
+                db.setPort(int(uri.port()))
+                db.setUserName(uri.username())
+                db.setPassword(uri.password())
+                # open (create) the connection
+                if db.open():
+                    print "Opened %s" % uri.uri()
+                    # execute a simple query
+                    #query = db.exec_("""select genus,species from botanical_name where genus = 'Prunus'""")
+                    query = QSqlQuery ("""select genus,species from botanical_name where genus = 'Prunus'""")
+                    self.dlg.tblPlants.clear()
+                    self.dlg.tblPlants.setRowCount(query.size())
+                    self.dlg.tblPlants.setColumnCount(query.record().count())
+                    self.dlg.tblPlants.setHorizontalHeaderLabels(["Genus", "Species"])
+                    self.dlg.tblPlants.setSelectionMode(QTableWidget.SingleSelection)
+                    self.dlg.tblPlants.setSelectionBehavior(QTableWidget.SelectRows)
+                    # loop through the result set and print the name
+                    index=0
+                    while query.next():
+                        record = query.record()
+                        self.dlg.tblPlants.setItem(index, 0, QTableWidgetItem(query.value(0)))
+                        self.dlg.tblPlants.setItem(index, 1, QTableWidgetItem(query.value(1)))
+                        index = index+1
+                        # print record.field('name').value().toString()
+                    self.dlg.tblPlants.resizeColumnsToContents()
+                else:
+                    err = db.lastError()
+                    print err.driverText()
+        # This piece of Python code is part of a plug-in that I wrote to QGis.
+        # This code allows you to populate an object QTableWidget.
+        # List contains a recordset query output with 2 fields
+        # The first 6 lines of code are for:
+        # - delete the contents of the object
+        # - determine the number of rows
+        # - set the number of columns
+        # - insert the header object
+        # - enable selection of the entire row
+        # FOR the next served instead to populate the object with the contents of the recordset. Note that the object is populated cell by cell.
+        # The last statement is used to resize the columns to content.
+
+
+        # plantlist = [["Malus","Domestica"],["Prunus", "armeniaca"],["Prunus","Communis"]]
+        # self.dlg.tblPlants.clear()
+        # self.dlg.tblPlants.setRowCount(len(plantlist))
+        # self.dlg.tblPlants.setColumnCount(2)
+        # self.dlg.tblPlants.setHorizontalHeaderLabels(["Genus", "Species"])
+        # self.dlg.tblPlants.setSelectionMode(QTableWidget.SingleSelection)
+        # self.dlg.tblPlants.setSelectionBehavior(QTableWidget.SelectRows)
+        #
+        # for i, plant in enumerate(plantlist):
+        #     genus = plant[0]
+        #     item = QTableWidgetItem(genus)
+        #     #selected = item
+        #     #item.setData(Qt.UserRole, QVariant(long(id(plant))))
+        #     self.dlg.tblPlants.setItem(i, 0, item)
+        #
+        #
+        #     species = plant[1]
+        #     item = QTableWidgetItem(species)
+        #     self.dlg.tblPlants.setItem(i, 1, item)
+        # pass
+        #
+        # self.dlg.tblPlants.resizeColumnsToContents()
 
 
     def selectFeature(self, point, button):
         # reset selection list on each new selection
-        self.selectList = []
+        self.phList = []
+        self.MoistureList = []
         pntGeom = QgsGeometry.fromPoint(point)
         pntBuff = pntGeom.buffer((self.canvas.mapUnitsPerPixel() * 2), 0)
         rect = pntBuff.boundingBox()
-        if self.cLayer:
-            rq = QgsFeatureRequest(rect)
-            for feat in self.cLayer.getFeatures(rq):
-                if feat.geometry().intersects(pntBuff):
-                    self.selectList.append(feat.id())
-            self.cLayer.setSelectedFeatures(self.selectList)
-            if self.selectList:
-                # make the actual selection
-                self.cLayer.setSelectedFeatures([self.selectList[0]])
-                # update the TextBrowser
-                self.updateTextBrowser()
+        #if self.pHLayer:
+        rq = QgsFeatureRequest(rect)
+        for feat in self.pHLayer.getFeatures(rq):
+            if feat.geometry().intersects(pntBuff):
+                self.phList.append(feat.id())
+        for feat in self.MoistureLayer.getFeatures(rq):
+            if feat.geometry().intersects(pntBuff):
+                self.MoistureList.append(feat.id())
+        if self.phList or self.MoistureList:
+            self.updateTextBrowser()
         else:
-                QMessageBox.information( self.iface.mainWindow(),"Info", "No layer currently selected in TOC" )
+                QMessageBox.information(self.iface.mainWindow(), "Info", "No pH layer currently selected in TOC")
 
 
     def run(self):
         """Run method that performs all the real work"""
         # set the current layer immediately if it exists, otherwise it will be set on user selection
-        self.cLayer = self.iface.mapCanvas().currentLayer()
-        #self.cLayer = QgsMapLayerRegistry.instance().mapLayer('pH')
-        if self.cLayer: self.provider = self.cLayer.dataProvider()
-
+        # self.cLayer = self.iface.mapCanvas().currentLayer()
+        self.pHLayer = QgsMapLayerRegistry.instance().mapLayersByName('pH')[0]
+        self.MoistureLayer = QgsMapLayerRegistry.instance().mapLayersByName('Moisture')[0]
         # make identify the tool we'll use 
         self.canvas.setMapTool(self.clickTool) 
         # show the dialog
